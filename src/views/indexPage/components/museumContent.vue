1a5ddd826629b977007dce7fa216398e8afaf661
Tarland2004 <2831116700@qq.com>
2024-10-06 14:56:52 +0800

    Update museumContent.vue

--- src/views/indexPage/components/museumContent.vue
+++ src/views/indexPage/components/museumContent.vue
@@ -7,9 +7,15 @@
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
 <template>
-    <div class="museum-content">
+    <div class="museum-content" style="padding-left: 2em;">
         <div class="content-html">
-            中国计量大学计量博物馆坐落于学校日月湖北岸，位于求是南楼4至5层。博物馆总建筑面积约为1700平方米，主展厅的建筑面积约1400平方米内部空间划分为公共活动区、展陈区、文物库房区、办公区等。累计投资1000余万元，收集馆藏文物近1000余件，通过各种实物、文字图片以万多媒体手段的应用，按历史发展时间顺序，集中展现了中国古代计量史和中国近现代计量史，诠释了计量文化的发展脉络和内涵精华，无论展馆定位、内涵还是展品种类、内容都位居全国计量专业博物馆前列,计量博物馆由古代度量衡和近现代计量两个展区构成。度量衡是中华民族的古老文明和智慧的结品，数千年深深植根于中华文明和民族的观念》惯中，广泛应用于生产、生活的各个领域，历史悠久，博大精深。四楼展厅是近现代计量部分，从明末清初传教士带来的西方科学导致的中国传统计量的变革谈起，揭示了近现代计量制度及管理的演变，民国时期统一度量衡、建立并推行近代计量制度的努力，时间计量的进展等，展示了新中国计量事业发展的历史进程，对新中国计量机构的演变，计量管理体系的法制化过程，计量教育、计量传播和计量科学研究、当代计量管理体系的特征等做了深入梳理。
+            <p>中国计量大学计量博物馆坐落于学校日月湖北岸，位于求是南楼4至5层。博物馆总建筑面积约为1700平方米，主展厅的建筑面积约1400平方米，内部空间划分为公共活动区、展陈区、文物库房区、办公区等。</p>
+
+            <p>累计投资1000余万元，收集馆藏文物近1000余件，通过各种实物、文字图片以多媒体手段的应用，按历史发展时间顺序，集中展现了中国古代计量史和中国近现代计量史，诠释了计量文化的发展脉络和内涵精华，无论展馆定位、内涵还是展品种类、内容都位居全国计量专业博物馆前列。</p>
+
+            <p>度量衡是中华民族的古老文明和智慧的结晶，数千年深深植根于中华文明和民族的观念习惯中，广泛应用于生产、生活的各个领域，历史悠久，博大精深。</p>
+
+            <p>四楼展厅是近现代计量部分，揭示了近现代计量制度及管理的演变，展示了新中国计量事业发展的历史进程等。</p>
         </div>
         <div class="museum-qrcode">
             <img src="../images/museum-qrcode.png" alt="" />
@@ -32,7 +38,8 @@ onUnmounted(() => {
 .museum-content {
     width: 100%;
     flex: 1;
-    padding: 2vh;
+    padding: 48px;
+    padding-top: 24px;
     box-sizing: border-box;
     position: relative;
 
@@ -40,6 +47,8 @@ onUnmounted(() => {
         max-height: 30vh;
         overflow: hidden;
         overflow-y: scroll;
+        font-family: 'NotoSansHans-Regular';
+        color: #1F4470;
     }
 
     .museum-qrcode {
@@ -47,26 +56,29 @@ onUnmounted(() => {
         flex-direction: column;
         align-items: center;
         font-weight: bold;
-        font-size: 1.3vh;
+        font-size: 18px;
+        font-family: 'NotoSansHans-Regular';
         color: #1F4470;
-        line-height: 3vh;
+        line-height: 18px;
         position: absolute;
-        left: 2vh;
-        bottom: 2vh;
+        left: 66px;
+        bottom: 40px;
 
         img {
-            height: 12vh;
-            width: 12vh;
+            height: 130px;
+            width: 130px;
+            position: absolute;
+            left: -18px;
+            bottom: 26px;
         }
     }
 
     .museum-experience-btn {
-        width: 16.7vh;
-        height: 4.4vh;
+        width: 180px;
+        height: 48px;
         position: absolute;
-        bottom: 9vh;
-        left: 50%;
-        transform: translateX(-50%);
+        bottom: 100px;
+        right: 440px;
         cursor: pointer;
     }
 }
